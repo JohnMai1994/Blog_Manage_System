5c15e1afcc994f3f7fd32b56bfa955c0cf17ec3e
Johnmai1994 <mjd64929@gmail.com>
2020-09-07 17:04:20 -0400

    完成SpringBoot和前端的基本整合

---
+++ src/main/java/com/jiadong/web/IndexController.java
@@ -0,0 +1,48 @@
+package com.jiadong.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/tags")
+    public String tags() {
+        return "tags";
+    }
+
+    @GetMapping("/types")
+    public String types() {
+        return "types";
+    }
+
+    @GetMapping("/archives")
+    public String archives() {
+        return "archives";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/blogs";
+    }
+
+    @GetMapping("/admin-input")
+    public String adminInput() {
+        return "admin/blogs-input";
+    }
+}
