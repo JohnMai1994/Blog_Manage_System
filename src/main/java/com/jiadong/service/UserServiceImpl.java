a82d8916150397921feb07f91ae70bbc6a4cd84e
Johnmai1994 <mjd64929@gmail.com>
2020-09-07 22:44:04 -0400

    MD5加密

--- src/main/java/com/jiadong/service/UserServiceImpl.java
+++ src/main/java/com/jiadong/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.jiadong.service;
 
 import com.jiadong.bean.User;
 import com.jiadong.dao.UserRepository;
+import com.jiadong.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
